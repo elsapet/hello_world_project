2fdbf55a4c913f11424e35152db5aa15e3ddb8b2
elizabeth <elsapet@users.noreply.github.com>
2015-07-24 16:55:09 +0200

    Update translate.rb

--- lib/hello_world_project/translate.rb
+++ lib/hello_world_project/translate.rb
@@ -2,8 +2,8 @@ module HelloWorldProject
 	class Translate
 
 		SUPPORTED_LANGUAGES = {
-			'dansk'			=> 	'hej verden',
-			'default' 	=> 	'hello world',
+			'dansk'		=> 	'hej verden',
+			'default' 	=> 	'01001000 01100101 01101100 01101100 01101111 00100000 01010111 01101111 01110010 01101100 01100100',
 			'deutsch' 	=> 	'guten tag',
 			'dinosaur' 	=> 	'rawr!',
 			'english' 	=> 	'hello world',
@@ -15,10 +15,10 @@ module HelloWorldProject
 
 		def initialize(language)
 			@language = if SUPPORTED_LANGUAGES.key? language
-										language.downcase
-									else
-										'default'
-									end
+				language.downcase
+			else
+				'default'
+			end
 		end
 
 		def say_hello
